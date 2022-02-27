ea9340eb6c8c527730c35f172a8cb9bd71590e03
Justin Heyes-Jones <justinhj@gmail.com>
2022-02-27 15:35:11 -0800

    move parallel render into a function

--- src/main.rs
+++ src/main.rs
@@ -34,29 +34,38 @@ fn main() {
 
     assert!(num_threads > 0);
 
-    println!("Proceeding with {:?} threads", num_threads);
+    parallel_render(&mut pixels, bounds, upper_left, lower_right, num_threads);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+}
+
+/// Given a pixel array, bounds and window co-ordinates and number of threads to use
+/// render to the pixel array
+fn parallel_render(
+    pixels: &mut Vec<u8>,
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+    num_threads: usize) {
+
+    println!("Parallel render with {:?} threads", num_threads);
 
     let rows_per_band = bounds.1 / num_threads + 1;
 
-    {
-        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
-        crossbeam::scope(|spawner| {
-            for (i, band) in bands.into_iter().enumerate() {
-                let top = rows_per_band * i;
-                let height = band.len() / bounds.0;
-                let band_bounds = (bounds.0, height);
-                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
-                let band_lower_right =
-                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
-                spawner.spawn(move |_| {
-                    render(band, band_bounds, band_upper_left, band_lower_right);
-                });
-            }
-        })
-        .unwrap();
-    }
-
-    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+    let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+    crossbeam::scope(|spawner| {
+        for (i, band) in bands.into_iter().enumerate() {
+            let top = rows_per_band * i;
+            let height = band.len() / bounds.0;
+            let band_bounds = (bounds.0, height);
+            let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+            let band_lower_right =
+                pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+            spawner.spawn(move |_| {
+                render(band, band_bounds, band_upper_left, band_lower_right);
+            });
+        }
+    }).unwrap();
 }
 
 /// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
