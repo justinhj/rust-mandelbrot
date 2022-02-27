ef77049760cf91a65f1ea28bd47a602525e426fe
Justin Heyes-Jones <justinhj@gmail.com>
2022-02-26 20:45:04 -0800

    Step 1 complete

---
+++ src/main.rs
@@ -0,0 +1,131 @@
+use num::Complex;
+use std::str::FromStr;
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+use std::env;
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT UPPERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x') .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]) .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]) .expect("error parsing lower right corner point");
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+
+}
+
+/// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
+/// iterations to decide.
+///
+/// If `c` is not a member, return `Some(i)`, where `i` is the number of
+/// iterations it took for `c` to leave the circle of radius 2 centered on the
+/// origin. If `c` seems to be a member (more precisely, if we reached the
+/// iteration limit without being able to prove that `c` is not a member),
+/// return `None`.
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { 
+        re: 0.0, 
+        im: 0.0 
+    }; 
+
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i)
+        }
+        z = z * z + c;
+    }
+    None
+}
+
+fn pixel_to_point(bounds: (usize,usize),
+                  pixel: (usize,usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>) -> Complex<f64> {
+    let (width,height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64 
+        // Why subtraction here? pixel.1 increases as we go down,
+        // but the imaginary component increases as we go up.
+    }
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) { 
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                    (Ok(l), Ok(r)) => Some((l, r)),
+                    _ => None }
+        } 
+    }
+}
+
+// Render the Mandelbrot set into the pixel array
+fn render(pixels: &mut [u8],
+          bounds: (usize,usize), 
+          upper_left: Complex<f64>,
+          lower_right: Complex<f64>) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for col in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (col, row), upper_left, lower_right);
+            pixels[row * bounds.0 + col] = 
+                match escape_time(point, 255) {
+                    None => 0,
+                    Some(count) => 255 - count as u8
+                }
+        }
+    }
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    let parsed: Option<(f64,f64)> = parse_pair(s, ',');
+    parsed.map(|(re,im)| Complex::new(re,im))
+}
+
+fn write_image(filename: &str, 
+            pixels: &[u8],
+            bounds: (usize,usize)) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+    Ok(())
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10,20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+#[test]
+fn test_parse_complex() { 
+    assert_eq!(parse_complex(&"1.25,-0.0625".to_string()), Some(Complex { re: 1.25, im: -0.0625 }));
+    assert_eq!(parse_complex(&",-0.0625".to_string()), None);
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 200), (25, 175),
+               Complex { re: -1.0, im:  1.0 },
+               Complex { re:  1.0, im: -1.0 }),
+        Complex { re: -0.5, im: -0.75 });
+
+}
